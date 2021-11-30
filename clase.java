afa901419d2f9cbbbb624a027e19981dd353ac4c
David-Duran <59368742+David-Duran@users.noreply.github.com>
2021-11-30 10:59:35 +0100

    new class

---
+++ clase.java
@@ -0,0 +1,15 @@
+public class Belly {
+private int cukes;
+public Belly() {
+this.cukes = 0;
+}
+public void eat(int cukes) {
+this.cukes += cukes;
+}
+public void digest(int hour) {
+this.cukes -= hour;
+}
+public int getCukes() {
+return cukes;
+}
+}
